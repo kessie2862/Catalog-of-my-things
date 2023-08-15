0e5ae9309b9c28fb4d85b249f1422dff4d5ef429
ANTOINE1128 <makdessyantoine@gmail.com>
2023-08-15 09:41:02 +0300

    fix linters

--- classes/game.rb
+++ classes/game.rb
@@ -1,19 +1,20 @@
+require_relative 'item'
+require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-   attr_accessor :multiplayer, :last_played_at
-
-  def initialize(id, title, author, source, label, publish_date, archived, multiplayer, last_played_at)
-    super(id, title, author, source, label, publish_date, archived)
+  def initialize(multiplayer, last_played, publish_date)
+    super(publish_date)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
-  end 
+    @last_played = last_played
+  end
 
   def can_be_archived?
-    super && (last_played_at < (Date.today - 365 * 2))
+    super && (last_played_at < (Date.today - (365 * 2)))
   end
+
   def add_item(item)
-     @items ||= [] # Initialize the collection if it doesn't exist
+    @items ||= [] # Initialize the collection if it doesn't exist
     @items << item
     item.game = self # Set the game property of the item
   end
