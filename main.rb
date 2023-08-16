2b739fdb227dfec40a7995e0f245b0a85d0d13cd
kessie <kayp3477@gmail.com>
2023-08-16 12:09:41 +0000

    Add entry options and case statements

--- main.rb
+++ main.rb
@@ -4,6 +4,12 @@ APP_OPTIONS = {
   '1' => 'Add a new book',
   '2' => 'List all books',
   '3' => 'List all labels',
+  '4' => 'Add a music album',
+  '5' => 'List all music albums',
+  '6' => 'List all genres',
+  '7' => 'Add a game',
+  '8' => 'List all games',
+  '9' => 'List all authors',
   '10' => 'Quit'
 }.freeze
 
@@ -28,16 +34,10 @@ class AppRunner
     loop do
       display_options
       user_choice = gets.chomp.to_s
-
-      case user_choice
-      when '1' then @app.add_book
-      when '10'
-        puts 'Hope you had fun. Hope to see you again 😊'
-        break
-      else
-        puts 'Invalid option ⛔. Please enter a number between 1 and 10'
-      end
+      break if process_choice(user_choice) == 'quit'
     end
+
+    puts 'Hope you had fun. Hope to see you again 😊'
   end
 
   private
@@ -45,6 +45,23 @@ class AppRunner
   def display_options
     puts 'Please choose an option by entering a number.'
     APP_OPTIONS.each { |index, string| puts "#{index} - #{string}" }
+  end
+
+  def process_choice(choice)
+    case choice
+    when '1' then @app.add_book
+    when '2' then @app.list_all_books
+    when '3' then @app.list_all_labels
+    when '4' then @app.add_music_album
+    when '5' then @app.list_all_music_albums
+    when '6' then @app.list_all_genres
+    when '7' then @app.add_game
+    when '8' then @app.list_all_games
+    when '9' then @app.list_all_authors
+    when '10' then 'quit'
+    else
+      puts 'Invalid option ⛔. Please enter a number between 1 and 10'
+    end
   end
 end
 
