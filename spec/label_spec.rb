a44d81b934f7c659600d3fa5b895b17a1357f9dc
ANTOINE1128 <makdessyantoine@gmail.com>
2023-08-17 17:43:25 +0300

    fixing test

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,26 +1,38 @@
 require_relative '../classes/Book/label'
-require_relative '../classes/item'
 
 RSpec.describe Label do
   describe '#add_item' do
     it 'adds an item to the label' do
       label = Label.new('Test Label', 'red')
-      item = Item.new(Date.new(2023, 8, 16))
+      item = double('item')
+
+      expect(item).to receive(:label=).with(label)
 
       label.add_item(item)
 
       expect(label.items).to include(item)
-      expect(item.label).to eq(label)
     end
+  end
 
-    it 'does not add the same item again' do
+  describe '#to_h' do
+    it 'returns a hash representation of the label' do
       label = Label.new('Test Label', 'red')
-      item = Item.new(Date.new(2023, 8, 16))
+      double('item', to_h: { id: 1, name: 'Item' })
 
-      label.add_item(item)
-      label.add_item(item)
+      # Creating a more complex test double that responds to label=
+      item_with_label_double = double('item_with_label', to_h: { id: 1, name: 'Item' })
+      allow(item_with_label_double).to receive(:label=)
 
-      expect(label.items).to eq([item])
+      label.add_item(item_with_label_double)
+
+      expected_hash = {
+        id: label.id,
+        title: 'Test Label',
+        color: 'red',
+        items: [{ id: 1, name: 'Item' }]
+      }
+
+      expect(label.to_h).to eq(expected_hash)
     end
   end
 end
